31979d78c53540443b7c7e90123ea8d30a7eb909
Brian Mavity <brian@brianmavity.com>
2010-08-31 17:22:29 -0500

    added sorting when clicked on header

--- jquery.caruso.grid.js
+++ jquery.caruso.grid.js
@@ -1,5 +1,6 @@
 (function($) {
-  var scrollbarWidth = $.getScrollbarWidth();
+  var scrollbarWidth = $.getScrollbarWidth(),
+      sortDataKey = 'caruso.grid.sortData';
 
   var createGrid = function(config) {
     var $grid = $('<div class="caruso-grid" />'),
@@ -7,8 +8,7 @@
         $head = $headerDiv.find('thead'),
         $bodyDiv = $('<div class="caruso-grid-body"><table><tbody></tbody></table></div>'),
         $bodyTable = $bodyDiv.find('table'),
-        $body = $bodyDiv.find('tbody'),
-        that = {};
+        $body = $bodyDiv.find('tbody');
 
     $grid.height(config.dimensions.height);
     $grid.width(config.dimensions.width);
@@ -28,19 +28,36 @@
       $.each(data, function() {
          $p.append(config.model.$dataRow.clone().inject(this));
       });
-      $body.append($p.children());
+      $body.empty().append($p.children());
       setColumnWidths();
     };
     
+    $headerDiv.click(function(evt) {
+      var $clickedHeader = $(evt.target).closest('th'),
+          sortItem = $clickedHeader.length && $clickedHeader.data(sortDataKey);
+
+      if(sortItem) {
+        sortItem.direction = sortItem.direction === 'asc' ? 'desc' : 'asc';
+        config.dataSource.getData(sortItem, setData);
+      };
+    });
+
     config.$placeholder.replaceWith($grid);
     config.dataSource.getData(setData);
-
-    return that;
   };
   
   var createModel = function($template) {
     var $headerRowTemplate = $template.find('table thead tr:first-child').clone(),
         $dataRowTemplate = $template.find('table tbody tr:first-child').clone();
+
+    $headerRowTemplate.children().each(function() {
+      var $this = $(this),
+          field = $this.attr('class');
+      $this.data(sortDataKey, {
+        field: field,
+        direction: 'asc'
+      });
+    });
 
     $dataRowTemplate.children().each(function(){
       $(this).html('');
