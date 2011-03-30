2da8cc8c6546d5fb49000332017a45956f06f108
Brian Mavity <brian@brianmavity.com>
2011-03-30 12:32:58 -0500

    Added injection for tag names

--- test/injectorSpecs.js
+++ test/injectorSpecs.js
@@ -1,14 +1,46 @@
 var vows = require('vows'),
     assert = require('assert');
 
-vows.describe('injector').addBatch({
+var suite = vows.describe('injector');
+
+suite.addBatch({
   'when initiated': {
     topic: function() {
       return require('caruso').injector;
     },
 
-    'should not bomb': function(injector) {
+    'should not blow up': function(injector) {
       assert.notEqual(injector, undefined);
     }
   }
-}).run();
+});
+
+suite.addBatch({
+  'when creating an environment': {
+    topic: function() {
+      var inj = require('caruso').injector;
+      inj.env('<div></div>', this.callback);
+    },
+
+    'should have the environment': function(err, env) {
+      assert.notEqual(env, undefined);
+    }
+  }
+});
+
+suite.addBatch({
+  'when injecting element content': {
+    topic: function() {
+      var inj = require('caruso').injector;
+      inj.env('<div></div>', this.callback);
+    },
+
+    'should have the environment': function(err, env) {
+      env.inject({ div: 'hi there' });
+      assert.ok(env.render().indexOf('<div>hi there</div>') !== -1);
+    }
+  }
+});
+
+
+suite.run();
